e64fe63bf46e6ae4807dcd219dc00f0fb50640d9
William Chamberlain <wzchamberlain@gmail.com>
2023-04-02 23:11:52 -0400

    Candidate ChartExportHelper.

--- src/ioutil/ChartExportHelper.java
+++ src/ioutil/ChartExportHelper.java
@@ -2,8 +2,14 @@ package ioutil;
 
 import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
+
+import javax.imageio.ImageIO;
 
 import comp.Centroid;
 import comp.Point;
@@ -40,8 +46,15 @@ public class ChartExportHelper {
 	 * @return The current singleton of ChartExportHelper.
 	 */
 	public static ChartExportHelper getInstance() {
-		Color[] temp = null;
-		// TODO: define default colour palette
+		Color[] temp = new Color[8];
+		temp[0] = Color.BLUE;
+		temp[1] = Color.CYAN;
+		temp[2] = Color.GREEN;
+		temp[3] = Color.MAGENTA;
+		temp[4] = Color.ORANGE;
+		temp[5] = Color.PINK;
+		temp[6] = Color.RED;
+		temp[7] = Color.YELLOW;
 		return getInstance(temp);
 	}
 	
@@ -53,9 +66,17 @@ public class ChartExportHelper {
 	 * @param links A boolean flag to draw links between centroids and their nearest points.
 	 * @param zones A boolean flag to draw a background of the centroids and their nearest points.
 	 * @param labels A boolean flag to draw the IDs of the points and centroids.
+	 * @throws IOException 
 	 */
-	public void exportAsCharts(File dest, Point[] pnts, List<Centroid[]> cntHistory, boolean links, boolean zones, boolean labels) {
-		// TODO: write code to export charts using other provided methods.
+	public void exportAsCharts(File dest, Point[] pnts, List<Centroid[]> cntHistory, boolean links, boolean zones, boolean labels) throws IOException {
+		for(int i = 1; i <= cntHistory.size(); i++) {
+			File f = new File(dest.getPath() + "(" + i + ").png");
+			Canvas can = generateChart(800,800, pnts, cntHistory.get(i - 1), links, zones, labels);
+			BufferedImage img = new BufferedImage(can.getWidth(), can.getHeight(), BufferedImage.TYPE_INT_RGB);
+			Graphics g = img.getGraphics();
+			can.paint(g);
+			ImageIO.write(img, "png", f);
+		}
 	}
 	
 	/**
@@ -68,8 +89,84 @@ public class ChartExportHelper {
 	 * @param labels A boolean flag to draw the IDs of the points and centroids.
 	 * @return The provided Canvas object with the newly drawn chart.
 	 */
-	public Canvas generateChart(Canvas canvas, Point[] pnts, Centroid[] cnt, boolean links, boolean zones, boolean labels) {
-		// TODO: write code to redraw chart onto provided canvas
+	public Canvas generateChart(Canvas canvas, Point[] pnts, Centroid[] cnts, boolean links, boolean zones, boolean labels) {
+		canvas.setBackground(Color.WHITE);
+		// find scaling factors
+		double xScl, xOff, yScl, yOff;
+		double low = Double.MAX_VALUE, high = Double.MIN_VALUE;
+		for(Point pnt : pnts) {
+			if(pnt.getX() < low)
+				low = pnt.getX();
+			if(pnt.getX() > high)
+				high = pnt.getX();
+		}
+		for(Point pnt : cnts) {
+			if(pnt.getX() < low)
+				low = pnt.getX();
+			if(pnt.getX() > high)
+				high = pnt.getX();
+		}
+		xScl = canvas.getWidth() / (high - low);
+		xOff = (0 - low) * xScl;
+		low = Double.MAX_VALUE;
+		high = Double.MIN_VALUE;
+		for(Point pnt : pnts) {
+			if(pnt.getY() < low)
+				low = pnt.getY();
+			if(pnt.getY() > high)
+				high = pnt.getY();
+		}
+		for(Point pnt : cnts) {
+			if(pnt.getY() < low)
+				low = pnt.getY();
+			if(pnt.getY() > high)
+				high = pnt.getY();
+		}
+		yScl = canvas.getWidth() / (high - low);
+		yOff = (0 - low) * yScl;
+		
+		// zones will likely be unimplemented
+		Graphics g = canvas.getGraphics();
+		g.clearRect(0, 0, canvas.getWidth(), canvas.getWidth());
+		if(zones) {
+		}
+		
+		//now for the points
+		g.setColor(Color.BLACK);
+		for(Point pnt : pnts) {
+			int x = (int) ((pnt.getX() - xOff) * xScl);
+			int y = (int) ((pnt.getY() - yOff) * yScl);
+			g.fillOval(x - 2, y - 2, 4, 4);
+		}
+		
+		//now for the centroids
+		for(int i = 0; i < cnts.length; i++) {
+			g.setColor(PALETTE[i % (PALETTE.length - 1)]);
+			int x = (int) ((cnts[i].getX() - xOff) * xScl);
+			int y = (int) ((cnts[i].getY() - yOff) * yScl);
+			g.fillRect(x - 2, y - 2, 4, 4);
+		}
+		
+		//now for the links
+		if(links) {
+			for(int i = 0; i < cnts.length; i++) {
+				g.setColor(PALETTE[i % (PALETTE.length - 1)]);
+				int x = (int) ((cnts[i].getX() - xOff) * xScl);
+				int y = (int) ((cnts[i].getY() - yOff) * yScl);
+				for(Point pnt : cnts[i].getOwned()) {
+					int x1 = (int) ((pnt.getX() - xOff) * xScl);
+					int y1 = (int) ((pnt.getY() - yOff) * yScl);
+					g.drawLine(x1, y1, x, y);
+				}
+			}
+		}
+		
+		//now for the lables, leave unimplemented for now
+		if(labels) {
+		}
+		
+		canvas.paint(g);
+		
 		return canvas;
 	}
 	
@@ -85,7 +182,8 @@ public class ChartExportHelper {
 	 * @return The generated Canvas ofject with the newly drawn chart.
 	 */
 	public Canvas generateChart(int xSize, int ySize, Point[] pnts, Centroid[] cnt, boolean links, boolean zones, boolean labels) {
-		// TODO: write code to generate new canvas and then call other provided method.
-		return new Canvas();
+		Canvas temp = new Canvas();
+		temp.setSize(xSize, ySize);
+		return generateChart(temp, pnts, cnt, links, zones, labels);
 	}
 }
